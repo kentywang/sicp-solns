c065ead00451b8b880eb92cbfc6004ba9a45fdb0
Kenty Wang <kentymail@gmail.com>
2019-05-13 08:02:06 -0400

    Start 2-6.

---
+++ 2-6.rkt
@@ -0,0 +1,24 @@
+#lang sicp
+
+(define zero (lambda (f) (lambda (x) x)))
+
+(define (add-1 n)
+  (lambda (f) (lambda (x) (f ((n f) x)))))
+
+;; const zero = f => x => x
+;; const addOne = n => f => x => f(n(f)(x))
+
+;; Deriving one from (add-1 zero).
+
+(lambda (f) (lambda (x) (f (((lambda (f) (lambda (x) x)) f) x))))
+(lambda (f) (lambda (x) (f ((lambda (x) x) x))))
+(lambda (f) (lambda (x) (f x)))
+
+;; Deriving one from (add-1 one).
+
+(lambda (f) (lambda (x) (f (((lambda (f) (lambda (x) (f x))) f) x))))
+(lambda (f) (lambda (x) (f ((lambda (x) (f x)) x))))
+(lambda (f) (lambda (x) (f (f x))))
+
+(define (+ a b)
+  (lambda (f)
