0a0cc69ff2bfbe13e5485be081ec915ce755a6b4
Kenty Wang <kentymail@gmail.com>
2019-05-15 07:17:09 -0400

    2-6

--- 2-6.rkt
+++ 2-6.rkt
@@ -1,24 +1,51 @@
 #lang sicp
 
-(define zero (lambda (f) (lambda (x) x)))
+;;; Try 1:
+
+(define zero
+  (lambda (x) x))
+
+(define one
+  (lambda (x) (f x)))
+
+;; Problem: If we are to represent integers as repeated applications of some function, then that function must be passed into the integer representation.
+
+;;; Try 2:
+
+(define zero
+  (lambda (f x) x))
+
+(define one
+  (lambda (f x) (f x)))
+
+;; We don't want to make higher-value integers into functions that are higher-order than smaller numbers, because that makes it hard to use the same operations on different numbers. We just want bigger numbers to have more fs applied to them.
+
+;; Thus, to apply an operation on a number, we must unwrap it, modify the number, and wrap it back up.
 
 (define (add-1 n)
-  (lambda (f) (lambda (x) (f ((n f) x)))))
+  (lambda (f x)
+    (f (n f x))))
 
-;; const zero = f => x => x
-;; const addOne = n => f => x => f(n(f)(x))
+;; Think this works. Now let’s try the addition operation.
 
-;; Deriving one from (add-1 zero).
+(define (+ a b)
+  (lambda (f x)
+    ((a f x) (b f x))))
 
-(lambda (f) (lambda (x) (f (((lambda (f) (lambda (x) x)) f) x))))
-(lambda (f) (lambda (x) (f ((lambda (x) x) x))))
-(lambda (f) (lambda (x) (f x)))
+;; This probably wouldn't work because we unwrap a to the base level, where it's no longer a procedure that can be applied.
 
-;; Deriving one from (add-1 one).
+(define (+ a b)
+  (lambda (f x)
+    (a f (b f x))))
 
-(lambda (f) (lambda (x) (f (((lambda (f) (lambda (x) (f x))) f) x))))
-(lambda (f) (lambda (x) (f ((lambda (x) (f x)) x))))
-(lambda (f) (lambda (x) (f (f x))))
+;; This seems to work, too. So it looks like we can rewrite the definitions to return a lambda that takes a function and a value instead of a function that returns a lambda that returns a value. At least for the current operations, it seems we don't need currying.
+
+;; Rewriting for the currying though:
 
 (define (+ a b)
   (lambda (f)
+    (lambda (x)
+      ((a f) ((b f) x)))))
+
+;; Edit: There's a neat rewrite online that didn't even need the x values in the expressions (including zeroes?):
+http://community.schemewiki.org/?sicp-ex-2.6
