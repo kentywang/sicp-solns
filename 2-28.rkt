c072955aa35af528d986a57c74d6d4ca411f1766
Kenty Wang <kentymail@gmail.com>
2019-05-22 08:32:09 -0400

    2-29

--- 2-28.rkt
+++ 2-28.rkt
@@ -16,7 +16,7 @@
 
 (fringe (list (list 1 (list 2 3)) 4))
 
-(define x 
+(define x
   (list (list 1 2) (list 3 4)))
 
 (fringe x)
@@ -26,12 +26,12 @@
 ;; Edit: There's actually a way to do in constant time without append,
 ;; using nested recursion to reverse traverse and cons to build parts
 ;; of the list piecewise (list would not work, since it would nest):
-(define (fringe L) 
-  (define (fringe-help L result) 
-    (if (null? L) ; if at end of the branch 
-        result 
-        (if (list? L) ; if the element is a list, not a number 
-            (fringe-help (car L) ; left branch 
-                         (fringe-help (cdr L) result)) ; right branch 
-            (cons L result)))) ; otherwise gather numbers into a list 
+(define (fringe L)
+  (define (fringe-help L result)
+    (if (null? L) ; if at end of the branch
+        result
+        (if (list? L) ; if the element is a list, not a number
+            (fringe-help (car L) ; left branch
+                         (fringe-help (cdr L) result)) ; right branch
+            (cons L result)))) ; otherwise gather numbers into a list
   (fringe-help L '()))
---
+++ 2-29.rkt
@@ -0,0 +1,74 @@
+#lang sicp
+
+;;; Deps
+
+(define (make-mobile2 left right)
+  (list left right))
+
+(define (make-branch2 length structure)
+  (list length structure))
+
+;;; 1.
+
+(define left-branch car) ;; Or (list-ref 0).
+(define right-branch cdr) ;; Or (list-ref 1).
+
+(define branch-length car)
+(define branch-structure cdr)
+
+;;; 2.
+
+;; Using a helper inner def
+(define (total-weight mobile)
+  (define (branch-weight branch)
+    (let ((structure (branch-structure branch)))
+      (if (pair? structure)
+          (+ (branch-weight (left-branch structure))
+             (branch-weight (right-branch structure)))
+          structure)))
+  (+ (branch-weight (left-branch mobile))
+     (branch-weight (right-branch mobile))))
+
+;; time = O(n)
+;; space = O(n)
+
+;;; 3.
+
+(define (torque branch)
+  (let ((structure (branch-structure branch)))
+    (* (branch-length branch)
+       (if (pair? structure)
+           (total-weight structure)
+           structure))))
+
+;; Trying without a helper inner def
+(define (balanced? mobile)
+  (if (pair? mobile)
+      (let ((l (left-branch mobile))
+            (r (right-branch mobile)))
+        (and (= (torque l)
+                (torque r))
+             (and (balanced? (branch-structure l))
+                  (balanced? (branch-structure r)))))
+        true)) ;; Weight is always balanced.
+
+;; time = O(n^2), since redundant torque calls.
+;; space = O(n)
+
+;;; 4. If we changed the constructors to this:
+
+(define (make-mobile left right)
+  (cons left right))
+
+(define (make-branch length structure)
+  (cons length structure))
+
+;; The only thing we'd need to modify with the selectors is changing the cadr
+;; to cdr. Everything else stays the same.
+
+;;; Tests
+(define x (make-mobile (make-branch 9 3)
+                       (make-branch 3 (make-mobile (make-branch 10 3)
+                                                   (make-branch 5 6)))))
+
+;; [(2L 3W) (3L [(3L 4W) (5L 6W)])] mobiles are brackets, branches are parens
