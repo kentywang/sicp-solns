92a1bd47bc7a38c74c64132d9a067a9b3968f222
Kenty Wang <kentymail@gmail.com>
2019-07-14 15:28:11 -0400

    4-19

---
+++ ch4/4-19.rkt
@@ -0,0 +1,13 @@
+;; If the specs for the language are to allow internal definitions to behave as
+;; if they were simultaneous defined (or perhaps or formally, as if the evaluation
+;; of each definition was at a point in time where all other definitions were
+;; already evaluated), then Eva is most correct. However, that may be impossible.
+;; Here's an example of the challenge:
+
+(define (f x)
+  (define a (+ 1 b))
+  (define b (+ 1 a))
+  (+ x a b))
+
+;; Here, a and b are mutually dependent values (and not procedures). This is a
+;; paradox, so Alyssa's approach is the most sane because it errors out.
