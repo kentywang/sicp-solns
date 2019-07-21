d1050f837185e2a9a8486e147578c730bcdc9220
Kenty Wang <kentymail@gmail.com>
2019-07-21 08:36:33 -0400

    4-42

---
+++ ch4/4-42.rkt
@@ -0,0 +1,42 @@
+#lang sicp
+
+
+;;; Deps
+
+(define (require p)
+  (if (not p) (amb)))
+
+(define (distinct? items)
+  (cond ((null? items) true)
+        ((null? (cdr items)) true)
+        ((member (car items) (cdr items)) false)
+        (else (distinct? (cdr items)))))
+
+(define (xor a b)
+  (not (eq? a b)))
+
+;;; 4.42
+
+;; amb makes this easy!
+
+(define (results)
+  (let ((betty (amb 1 2 3 4 5))
+        (ethel (amb 1 2 3 4 5))
+        (joan (amb 1 2 3 4 5))
+        (kitty (amb 1 2 3 4 5))
+        (mary (amb 1 2 3 4 5)))
+    (require
+     (distinct? (list betty ethel joan kitty mary)))
+    (require (xor (= kitty 5) (= betty 3)))
+    (require (xor (= ethel 1) (= joan 2)))
+    (require (xor (= joan 3) (= ethel 5)))
+    (require (xor (= kitty 2) (= mary 4)))
+    (require (xor (= mary 4) (= betty 1)))
+    (list (list 'betty betty)
+          (list 'ethel ethel)
+          (list 'joan joan)
+          (list 'kitty kitty)
+          (list 'mary mary))))
+
+; ((betty 2) (ethel 1) (joan 3) (kitty 5) (mary 4))
+
