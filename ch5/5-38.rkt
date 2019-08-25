61afb7e6667253c7c6a656b24280bbbe83b90dbb
Kenty Wang <kentymail@gmail.com>
2019-08-25 11:20:28 -0400

    5-38

--- ch5/5-38.rkt
+++ ch5/5-38.rkt
@@ -2,10 +2,11 @@
 
 ;;; New
 
-;; 1.
+;;; 1.
+
 (define (spread-arguments operands)
-  ;; This took me 12-16hrs!! Basically, the first operand could modify
-  ;; the env and continue, so we need to preserve it.
+  ;; This took me 16 hours! Basically, the first operand could modify
+  ;; the env and continue, so we need to preserve them.
   (preserving '(env continue)
    (compile (car operands) 'arg1 'next)
    (preserving
@@ -13,18 +14,19 @@
     (compile (cadr operands) 'arg2 'next)
     (make-instruction-sequence '(arg1) '() '())))) ; Not the nicest way to do this...
 
-;; 2.
+;;; 2.
+
 (define (compile-= exp target linkage)
   (let ((operands (cdr exp)))
     (if (not (= 2 (length operands)))
         (error "Operand list length not correct, should be two." operands)
         (end-with-linkage
          linkage
-         (preserving '(continue)
-                     (spread-arguments operands)
-                     (make-instruction-sequence
-                      '(arg1 arg2) `(,target)
-                      `((assign ,target (op =) (reg arg1) (reg arg2)))))))))
+         (append-instruction-sequences
+          (spread-arguments operands)
+          (make-instruction-sequence
+           '(arg1 arg2) `(,target)
+           `((assign ,target (op =) (reg arg1) (reg arg2)))))))))
 
 (define (compile-- exp target linkage)
   (let ((operands (cdr exp)))
@@ -32,38 +34,52 @@
         (error "Operand list length not correct, should be two." operands)
         (end-with-linkage
          linkage
-         (preserving '(continue)
-                     (spread-arguments operands)
-                     (make-instruction-sequence
-                      '(arg1 arg2) `(,target)
-                      `((assign ,target (op -) (reg arg1) (reg arg2)))))))))
-
-(define (compile-* exp target linkage)
-  (display exp)
-  (display target)
-  (display linkage)
-  (let ((operands (cdr exp)))
-    (if (not (= 2 (length operands)))
-        (error "Operand list length not correct, should be two." operands)
-        (end-with-linkage
-         linkage
-         (preserving '(continue)
-                     (spread-arguments operands)
-                     (make-instruction-sequence
-                      '(arg1 arg2) `(,target)
-                      `((assign ,target (op *) (reg arg1) (reg arg2)))))))))
-
-(define (compile-+ exp target linkage)
-  (let ((operands (cdr exp)))
-    (if (not (= 2 (length operands)))
-        (error "Operand list length not correct, should be two." operands)
-        (end-with-linkage
-         linkage
-         (preserving '(continue)
-                     (spread-arguments operands)
-                     (make-instruction-sequence
+         (append-instruction-sequences
+          (spread-arguments operands)
+          (make-instruction-sequence
            '(arg1 arg2) `(,target)
-           `((assign ,target (op +) (reg arg1) (reg arg2)))))))))
+           `((assign ,target (op -) (reg arg1) (reg arg2)))))))))
+;; 4.
+(define (compile-* exp target linkage)
+  (let* ((operands (cdr exp))
+         (len (length operands)))
+    (cond ((< len 2)
+           (error "Less than 2 operands." operands))
+          ((> len 2)
+           ;; Recursively call with 1 fewer operand.
+           (compile-*
+            `(* (* ,(car operands) ,(cadr operands)) . ,(cddr operands))
+            target
+            linkage))
+          (else
+           (end-with-linkage
+            linkage
+            (append-instruction-sequences
+             (spread-arguments operands)
+             (make-instruction-sequence
+              '(arg1 arg2) `(,target)
+              `((assign ,target (op *) (reg arg1) (reg arg2))))))))))
+
+;; 4.
+(define (compile-+ exp target linkage)
+  (let* ((operands (cdr exp))
+         (len (length operands)))
+    (cond ((< len 2)
+           (error "Less than 2 operands." operands))
+          ((> len 2)
+           ;; Recursively call with 1 fewer operand.
+           (compile-+
+            `(+ (+ ,(car operands) ,(cadr operands)) . ,(cddr operands))
+            target
+            linkage))
+        (else
+         (end-with-linkage
+          linkage
+          (append-instruction-sequences
+           (spread-arguments operands)
+           (make-instruction-sequence
+            '(arg1 arg2) `(,target)
+            `((assign ,target (op +) (reg arg1) (reg arg2))))))))))
 
 (define (compile exp target linkage)
   (cond ((self-evaluating? exp)
@@ -90,6 +106,137 @@
          (compile-application exp target linkage))
         (else
          (error "Unknown expression type -- COMPILE" exp))))
+
+;;; 3.
+
+;;; Compare the original compiled code:
+;
+;(assign val (op make-compiled-procedure) (label entry1) (reg env))
+;(goto (label after-lambda2))
+;entry1
+;(assign env (op compiled-procedure-env) (reg proc))
+;(assign env (op extend-environment) (const (n)) (reg argl) (reg env))
+;(save continue)
+;(save env)
+;(assign proc (op lookup-variable-value) (const =) (reg env))
+;(assign val (const 1))
+;(assign argl (op list) (reg val))
+;(assign val (op lookup-variable-value) (const n) (reg env))
+;(assign argl (op cons) (reg val) (reg argl))
+;(test (op primitive-procedure?) (reg proc))
+;(branch (label primitive-branch6))
+;compiled-branch7
+;(assign continue (label after-call8))
+;(assign val (op compiled-procedure-entry) (reg proc))
+;(goto (reg val))
+;primitive-branch6
+;(assign val (op apply-primitive-procedure) (reg proc) (reg argl))
+;after-call8
+;(restore env)
+;(restore continue)
+;(test (op false?) (reg val))
+;(branch (label false-branch4))
+;true-branch3
+;(assign val (const 1))
+;(goto (reg continue))
+;false-branch4
+;(assign proc (op lookup-variable-value) (const *) (reg env))
+;(save continue)
+;(save proc)
+;(assign val (op lookup-variable-value) (const n) (reg env))
+;(assign argl (op list) (reg val))
+;(save argl)
+;(assign proc (op lookup-variable-value) (const factorial) (reg env))
+;(save proc)
+;(assign proc (op lookup-variable-value) (const -) (reg env))
+;(assign val (const 1))
+;(assign argl (op list) (reg val))
+;(assign val (op lookup-variable-value) (const n) (reg env))
+;(assign argl (op cons) (reg val) (reg argl))
+;(test (op primitive-procedure?) (reg proc))
+;(branch (label primitive-branch9))
+;compiled-branch10
+;(assign continue (label after-call11))
+;(assign val (op compiled-procedure-entry) (reg proc))
+;(goto (reg val))
+;primitive-branch9
+;(assign val (op apply-primitive-procedure) (reg proc) (reg argl))
+;after-call11
+;(assign argl (op list) (reg val))
+;(restore proc)
+;(test (op primitive-procedure?) (reg proc))
+;(branch (label primitive-branch12))
+;compiled-branch13
+;(assign continue (label after-call14))
+;(assign val (op compiled-procedure-entry) (reg proc))
+;(goto (reg val))
+;primitive-branch12
+;(assign val (op apply-primitive-procedure) (reg proc) (reg argl))
+;after-call14
+;(restore argl)
+;(assign argl (op cons) (reg val) (reg argl))
+;(restore proc)
+;(restore continue)
+;(test (op primitive-procedure?) (reg proc))
+;(branch (label primitive-branch15))
+;compiled-branch16
+;(assign val (op compiled-procedure-entry) (reg proc))
+;(goto (reg val))
+;primitive-branch15
+;(assign val (op apply-primitive-procedure) (reg proc) (reg argl))
+;(goto (reg continue))
+;after-call17
+;after-if5
+;after-lambda2
+;(perform (op define-variable!) (const factorial) (reg val) (reg env))
+;(assign val (const ok))
+
+;; Compare to the open-code primitives:
+;
+;  (assign val (op make-compiled-procedure) (label entry1) (reg env))
+;  (goto (label after-lambda2))
+;entry1
+;  (assign env (op compiled-procedure-env) (reg proc))
+;  (assign env (op extend-environment) (const (n)) (reg argl) (reg env))
+;  (assign arg1 (op lookup-variable-value) (const n) (reg env))
+;  (assign arg2 (const 1))
+;  (assign val (op =) (reg arg1) (reg arg2))
+;  (test (op false?) (reg val))
+;  (branch (label false-branch4))
+;true-branch3
+;  (assign val (const 1))
+;  (goto (reg continue))
+;false-branch4
+;  (save continue)
+;  (save env)
+;  (assign proc (op lookup-variable-value) (const factorial) (reg env))
+;  (assign arg1 (op lookup-variable-value) (const n) (reg env))
+;  (assign arg2 (const 1))
+;  (assign val (op -) (reg arg1) (reg arg2))
+;  (assign argl (op list) (reg val))
+;  (test (op primitive-procedure?) (reg proc))
+;  (branch (label primitive-branch6))
+;compiled-branch7
+;  (assign continue (label proc-return9))
+;  (assign val (op compiled-procedure-entry) (reg proc))
+;  (goto (reg val))
+;proc-return9
+;  (assign arg1 (reg val))
+;  (goto (label after-call8))
+;primitive-branch6
+;  (assign arg1 (op apply-primitive-procedure) (reg proc) (reg argl))
+;after-call8
+;  (restore env)
+;  (assign arg2 (op lookup-variable-value) (const n) (reg env))
+;  (assign val (op *) (reg arg1) (reg arg2))
+;  (restore continue)
+;  (goto (reg continue))
+;after-if5
+;after-lambda2
+;  (perform (op define-variable!) (const factorial) (reg val) (reg env))
+;  (assign val (const ok))
+
+;;; The difference is 78 LOC vs 41 LOC (47% decrease).
 
 ;;; Old
 
@@ -710,3 +857,7 @@
    (append (statements seq1) (statements seq2))))
 
 '(COMPILER LOADED)
+
+;;; 4. Tests:
+
+(compile '(+ 1 2 (* 3 4 5)) 'val 'next)
