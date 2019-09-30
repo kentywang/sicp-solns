8fb3f55e7e7709682e688f0caeb201c51596cfa3
Kenty Wang <kentymail@gmail.com>
2019-09-29 20:08:35 -0400

    Rudimentary Lisp reader.

--- ch5/5-51.c
+++ ch5/5-51.c
@@ -5,6 +5,8 @@
 
 #define MEMORYLIMIT 1000
 
+typedef enum type Type;
+typedef union value Value;
 typedef struct element Element;
 typedef struct pair Pair;
 
@@ -17,13 +19,12 @@ enum error_codes {
 // Be careful, though, since "initial values" means nothing after garbage
 // collection.
 struct element {
-  enum {
+  enum type {
     PAIR,
     NUMBER,
     SYMBOL
   } type_tag;
-
-  union {
+  union value {
     Pair *pair_ptr;
     double number;
     char *symbol;
@@ -39,7 +40,7 @@ struct pair {
 static Pair memory[MEMORYLIMIT];
 static Pair *free_ptr = memory;
 
-static Pair *exp;
+static Element exp;
 // static Environment *env;
 // static void *val;
 // static char continue;
@@ -59,6 +60,21 @@ in dynamically-allocated memory via malloc/free.
 
 Questions:
 - How do you differentiate between newlines (ignored) and the enter key?
+- I moved cdr assignment out from create_symbol (and create_pair), does it
+  still work? I'd think so, because it probably chained numbers outside of
+  parens, even when we only wanted that to happens within a set of
+  parenthesis.
+- Do we need to manually set CDR to null because of GC?
+- We don't store symbols/numbers not in a list in the list struct, right?
+
+Tests:
+  (1(  2  3  )4)
+   (1 2 3)
+    (1 2 (3 4 (5)) 6 7)
+
+Lessons learned:
+- For mutating an object's pointer member, I can't pass the pointer into
+  a function; I must pass the object.
 
 */
 
@@ -73,67 +89,96 @@ int main(int argc, char *argv[])
 
 void read_eval_print_loop(void)
 {
-  Pair *read_input(void);
-  void print_pair(Pair *);
+  void read_input(Element *);
+  void print_element(Element *);
 
   // Read
-  exp = read_input();
+  read_input(&exp);
 
   // Eval
   // env = get_global_environment();
   // val = eval_dispatch(exp, env);
   
   // Print
-  print_pair(exp/*val*/);
-  
+  print_element(&exp/*val*/);
+  printf("\n");
+
+  // Free memory step?
+
   // // Loop
   // read_eval_print_loop();
 }
 
-Pair *read_input(void)
+void read_input(Element *e)
 {
-  Pair *read_parens(void);
-  Pair *read_word(int);
+  void print_element(Element *);
+  void read_parens(Element *);
+  char *read_word(int);
 
   int c;
 
   // Skip over leading whitespace.
   while ((c = getch()) != EOF && isspace(c))
-    ;
+    printf("read_input\n  space\n");
+
+  // Do something with EOF here.
 
   // At start here, c is non-EOF, non-space char.
-  if (c == '(')
-    return read_parens();
-  else if (c != ')')
-    return read_word(1);
+  if (c == '(') {
+    printf("read_input\n  (\n");
+    e->type_tag = PAIR;
+    read_parens(e);
+
+    // printf("Finished reading input,\n");
+    // print_element(e);
+    // printf("\n");
+    return;
+  }
+  else if (c != ')') {
+    printf("read_input\n  %c\n", c);
+    e->type_tag = SYMBOL;
+    e->contents.symbol = read_word(1);
+    // What about numbers?
+    return;
+  }
 
   fprintf(stderr, "Bad identifier.\n");
   exit(BAD_IDENTIFIER);
 }
 
-Pair *read_word(int count)
+char *read_word(int count)
 {
-  Pair *create_symbol(int size);
+  char *create_symbol(int size);
 
-  int c;
+  int c = getch();
 
-  if ((c = getch()) == EOF || isspace(c))
-    return create_symbol(count);
+  if (
+    c == EOF ||
+    isspace(c) ||
+    c == '(' ||
+    c == ')'
+  ) {
+    printf("read_word\n  found EOF/WS/(/).\n");
 
-  if (c == '(' || c == ')') {
+    // Do something with EOF here.
+
     // Our job here is done. Return paren for another function to process.
+    // We need to return character to buffer because the count variable must
+    // be in sync.
     ungetch(c);
     return create_symbol(count);
   }
+  printf("read_word\n  character is: %c\n", c);
 
   return read_word(count + 1);
 }
 
-Pair *read_parens()
+void read_parens(Element *e)
 {
+  void print_pair(Pair *p);
+
   Pair *read_cdr(Pair *);
-  Pair *create_pair(Pair *);
-  Pair *p = NULL;
+  Pair *get_next_free_ptr(void);
 
   int c;
 
@@ -141,63 +186,80 @@ Pair *read_parens()
   while ((c = getch()) != EOF && isspace(c))
     ;
 
+  // Do something with EOF here.
+
   // At start here, c is non-EOF, non-space char.
   // This should handle empty lists, since p is initialized to NULL?
-  if (c == ')')
-    return p;
+  if (c == ')') {
+    printf("read_parens\n  )\n");
+    e->contents.pair_ptr = NULL;
+    return;
+  }
 
-  p = c == '(' ? create_pair(read_parens()) : read_word(1);
+  Pair *p = e->contents.pair_ptr = get_next_free_ptr();
+  
+  if (c == '(') {
+    printf("read_parens\n  (\n");
+    p->car.type_tag = PAIR;
+    read_parens(&p->car);
+  } else {
+    printf("read_parens\n  %c\n", c);
+    p->car.type_tag = SYMBOL;
+    p->car.contents.symbol = read_word(1);
 
-  // Continue with the cdr, but no need to assign it to anything since its
+    // We know read_word put ) in the buffer, so we skip over it.
+    getch();
+  }
+
+  // Continue with the cdr, but no need to assign it to anything since it's
   // already been done by set_next_free_ptr.
-  read_parens();
+  p->cdr.type_tag = PAIR;
+  read_parens(&p->cdr);
 
-  return p;
+  // printf("Finished reading between parens,\n");
+  // print_pair(p);
+  // printf("\n");
+
+  return;
 }
 
 /////////////////////////////////////////////////////////////////////////////
 
 Pair *get_next_free_ptr(void);
 Pair *set_next_free_ptr(void);
+void buffered_seek(int n);
 
-Pair *create_symbol(int size)
+char *create_symbol(int size)
 {
-  // Rewind stdin pointer to start of word.
-  fseek(stdin, -size, SEEK_CUR);
+  // Reserve memory size for word.
+  char *s = (char *) malloc(size + 1);
 
-  // Reserve memory size for word, saving ref to this location in memory
-  // in car of next free pair.
-  free_ptr->car.type_tag = SYMBOL;
-  free_ptr->car.contents.symbol = (char *) malloc(size + 1);
+  // Rewind stdin pointer to start of word.
+  buffered_seek(size);
 
   // Copy over word from stdin to free pair's car.
-  fgets(free_ptr->car.contents.symbol, size, stdin);
+  fgets(s, size + 1, stdin);
   // Will this handle empty strings?
-  *(free_ptr->car.contents.symbol + size) = '\0';
+  *(s + size) = '\0';
+  printf("create_symbol\n  \"%s\", size: %d\n", s, size);
 
-  free_ptr->cdr.type_tag = PAIR;
-  free_ptr->cdr.contents.pair_ptr = get_next_free_ptr();
-
-  return set_next_free_ptr();
-}
-
-Pair *create_pair(Pair *p)
-{
-  free_ptr->car.type_tag = PAIR;
-  free_ptr->cdr.contents.pair_ptr = get_next_free_ptr();
-
-  return set_next_free_ptr();
+  return s;
 }
 
 // When not full, just the next element in array. Otherwise need to garbage
 // collect.
 Pair *get_next_free_ptr(void)
 {
-  if (free_ptr + 1 < memory + MEMORYLIMIT)
-    return free_ptr + 1;
+  Pair *p = free_ptr;
+
+  if (free_ptr + 1 < memory + MEMORYLIMIT) {
+    free_ptr += 1;
+    return p;
+  }
 
   // WIP, need to implement garbage collection.
-  return free_ptr;
+  printf("Uh-oh.\n");
+  return p;
 }
 
 // Returns _old_ free pointer, while updating current free pointer. We can
@@ -229,14 +291,23 @@ void ungetch(int c)
   char_buffer = c;
 }
 
+void buffered_seek(int n)
+{
+  fseek(stdin, -n - (char_buffer ? 1 : 0), SEEK_CUR);
+}
+
+
 /////////////////////////////////////////////////////////////////////////////
 
 void print_pair(Pair *p)
 {
   void print_element(Element *);
 
+  printf("(");
   print_element(&p->car);
+  printf(" . ");
   print_element(&p->cdr);
+  printf(")");
 }
 
 void print_element(Element *e)
@@ -244,18 +315,20 @@ void print_element(Element *e)
   if (e) {
     switch (e->type_tag) {
     case PAIR:
-      printf("(\n");
-      print_pair(e->contents.pair_ptr);
-      printf(")\n");
+      if (e->contents.pair_ptr)
+        print_pair(e->contents.pair_ptr);
+      else
+        printf("nil");
       break;
     case NUMBER:
-      printf("%f\n", e->contents.number);
+      printf("%f", e->contents.number);
       break;
     case SYMBOL:
-      printf("%s\n", e->contents.symbol);
+      printf("%s", e->contents.symbol);
       break;
     }
-  }
-  // We shouldn't get here unless GC.
-  printf("Uh-oh.\n");
+    return;
+  } else
+    // We shouldn't get here unless GC.
+    printf("Uh-oh.\n");
 }
